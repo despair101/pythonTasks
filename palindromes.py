4eaf345b37545870486dc8553ccd0a175b74d9bb
despair101 <58655234+despair101@users.noreply.github.com>
2020-10-08 23:13:41 +0700

    Create palindromes.py

---
+++ palindromes.py
@@ -0,0 +1,11 @@
+def isPalindrome(nmb, i, j):
+    return str(nmb)[i - 1:j] == ''.join(reversed(list(str(nmb)[i - 1:j])))
+    
+for a in range(1, 10):
+    for b in range(10):
+        for c in range(10):
+                n = a * 100000 + b * 10000 + c * 1000 + c * 100 + b * 10 + a
+                if n > 999999:
+                    c = 10
+                elif isPalindrome(n - 3, 2, 6) and isPalindrome(n - 2, 2, 6) and isPalindrome(n - 1, 2, 4):
+                    print(n - 3)
